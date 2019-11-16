03d0893b3554438501d6769253bb773c19f059b6
松田 信介 <matsusin0413@gmail.com>
2019-11-16 10:52:53 +0900

    use class

--- js/main.js
+++ js/main.js
@@ -1,102 +1,128 @@
 "use strict";
 
-let gameController = new GameController();
-gameController.start();
-
-function GameController() {
-  this.MOGURAS = document.querySelectorAll(".mogura");
-  for (let i = 0; i < this.MOGURAS.length; i++) {
-    // オブジェクトの参照を渡している
-    new MoguraObj(this.MOGURAS[i], this);
-  }
-
-  // ゲーム開始
-  this.start = function () {
-    // モグラを一定間隔で生成する
-    this.moguraGenerate = setInterval(() => {
-      let rand = Math.floor(Math.random() * this.MOGURAS.length);
-      this.MOGURAS[rand].deru();
-    }, 500);
-  }
-
-  // ゲーム終了
-  this.finish = function () {
-    clearInterval(this.moguraGenerate);
-
-    for (let i = 0; i < this.MOGURAS.length; i++) {
+// ゲーム全体を制御するクラス
+class GameController {
+  constructor() {
+    this.moguraObjects = [];
+    let moguras = document.querySelectorAll(".mogura");
+    for (let i = 0; i < moguras.length; i++) {
       // オブジェクトの参照を渡している
-      this.MOGURAS[i].src = "";
+      this.moguraObjects.push(new MoguraObject(moguras[i], this))
     }
   }
 
-  this.countMogura = function () {
+  // ゲーム開始
+  start () {
+    // モグラを一定間隔で生成する
+    this.moguraGenerate = setInterval(() => {
+      let rand = Math.floor(Math.random() * this.moguraObjects.length);
+      this.moguraObjects[rand].deru();
+    }, 500);
+
+    // 残り秒数をカウントダウンする
+    this.gemeCountdown = setInterval((gameController) => {
+      gameController.finish();
+    }, 15000, this);
+  }
+
+  // ゲーム終了
+  finish () {
+    console.log("ゲーム終了")
+    clearInterval(this.moguraGenerate);
+    clearInterval(this.gemeCountdown)
+    for (let i = 0; i < this.moguraObjects.length; i++) {
+      // オブジェクトの参照を渡している
+      this.moguraObjects[i].image.src = "";
+    }
+  }
+
+  // 出現しているモグラの数を数える
+  countMogura () {
     let cnt = 0;
-    for (let i = 0; i < this.MOGURAS.length; i++) {
-      if (this.MOGURAS[i].status == 1) {
+    for (let i = 0; i < this.moguraObjects.length; i++) {
+      if (this.moguraObjects[i].status == 1) {
         cnt++;
       }
     }
     return cnt;
   }
-
-  setInterval((gameController) => {
-    gameController.finish();
-  }, 60000, this);
 }
 
-function MoguraObj(image, gameController) {
-  this.image = image;
-  image.status = 0; // 0:hide, 1:show, 2:press
-  image.autoHide = "";
-  image.MOGURA_TYPES = ["mogura", "gurasan", "gobu"];
-  image.MOGURA_IMAGES = {
-    mogura: ["", "./images/モグ2.png", "./images/モグ1.png"],
-    gurasan: ["", "./images/モグ3.png", "./images/モグ4.png"],
-    gobu: ["", "./images/ゴブ1.png", "./images/ゴブ2.png"]
+// モグラ１匹を制御するクラス
+class MoguraObject {
+
+  // 初期化処理
+  constructor(image, gameController) {
+    this.image = image;
+    this.image.data = this; // ポイント1
+    this.image.onclick = this.onclick;
+    this.gameController = gameController;
+    this.status = 0; // 0:hide, 1:show, 2:press
+    this.autoHide = "";
+    this.MOGURA_TYPES = ["mogura", "gurasan", "gobu"];
+    this.MOGURA_IMAGES = {
+      mogura: ["", "./images/モグ2.png", "./images/モグ1.png"],
+      gurasan: ["", "./images/モグ3.png", "./images/モグ4.png"],
+      gobu: ["", "./images/ゴブ1.png", "./images/ゴブ2.png"]
+    }
   }
 
-  image.onclick = function () {
-    if (image.status == 1) {
-      image.status = 2;
-      image.update();
+  onclick(){
+    this.data.press(); // ポイント2
+  }
 
-      clearTimeout(image.autoHide);
-      setTimeout(image.kakureru, 400, image);
-    }
+  press() {
+    if (this.status != 1) return;
+    
+    this.setStatus(2);
+
+    clearTimeout(this.autoHide);
+    setTimeout(() => { this.setStatus(0) }, 400);
   }
 
   // モグラが出現する
-  image.deru = function () {
-    if (image.status != 0) return false;
+  deru() {
+    if (this.status != 0) return false;
     if (Math.random() < 0.7) return false;
-    if (gameController.countMogura() >= 2) return false;
+    if (this.overMoguraDisplayLimit()) return false; // 同時に２匹までしか出ないようにする
 
-    image.moguraType = image.setImageType();
+    // どの種類のモグラかを決定する
+    this.moguraType = this.setImageType();
 
-    image.status = 1;
-    image.update();
-    image.autoHide = setTimeout(image.kakureru, (3000 * Math.random()) + 2000);
+    this.setStatus(1);
+
+    let hideIntervalTime = (3000 * Math.random()) + 2000;
+    this.autoHide = setTimeout(() => { this.setStatus(0) }, hideIntervalTime);
   }
 
-  // モグラが隠れる
-  image.kakureru = function () {
-    image.status = 0;
-    image.update();
+  // モグラの同時出現が上限に達していないかをチェックする
+  overMoguraDisplayLimit() {
+    return (this.gameController.countMogura() >= 2);
   }
 
-  image.update = function () {
-    image.src = image.MOGURA_IMAGES[image.moguraType][image.status];
+  // ステータスを更新する
+  setStatus (statusNumber) {
+    this.status = statusNumber; // 0:hide, 1:show, 2:press
+    this.update();
   }
 
-  // モグラの種類を決定する
-  image.setImageType = function () {
+  // モグラのタイプとステータスに応じて、画像を切り替える
+  update () {
+    this.image.src = this.MOGURA_IMAGES[this.moguraType][this.status];
+  }
+
+  // モグラの種類をランダムに決定する
+  setImageType () {
     let rand = Math.random();
     if (rand > 0.95) {
-      return image.MOGURA_TYPES[2]; // 0:もぐら 1:グラサン 2:ゴブリン
+      return this.MOGURA_TYPES[2]; // 0:もぐら 1:グラサン 2:ゴブリン
     } else if (rand > 0.8) {
-      return image.MOGURA_TYPES[1]; // 0:もぐら 1:グラサン 2:ゴブリン
+      return this.MOGURA_TYPES[1]; // 0:もぐら 1:グラサン 2:ゴブリン
     } else {
-      return image.MOGURA_TYPES[0]; // 0:もぐら 1:グラサン 2:ゴブリン
+      return this.MOGURA_TYPES[0]; // 0:もぐら 1:グラサン 2:ゴブリン
     }
   }
 }
+
+let gameController = new GameController();
+gameController.start();
